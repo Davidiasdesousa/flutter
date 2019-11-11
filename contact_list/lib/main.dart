37f8f72cc6d93b6c6c79466a5a9f3de8595bbf5f
Davi Dias de Sousa <davidiasdesousa@gmail.com>
2019-11-10 23:38:45 -0300

    list contacts

---
+++ contact_list/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:contact_list/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
